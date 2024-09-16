97129e2dee7b458304e6407d1dd51e6fcd40063f
Inayathasan0 <inayathasan82@gmail.com>
2024-09-17 00:47:53 +0530

    temporary models setup

---
+++ models/product.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+
+const productSchema = mongoose.Schema(
+    {
+        productname: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true
+        },
+        discount: {
+            type: Number,
+            default: 0
+        },
+        bgcolor: {
+            type: String,
+            default: ''
+        },
+        panelcolor: {
+            type: String,
+            default: ''
+        },
+        textcolor: {
+            type: String,
+            default: ''
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model('Product',productSchema);
---
+++ models/user.model.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const { stringify } = require('querystring');
+
+mongoose.connect(`mongodb://127.0.0.1:27017/megaproject`);
+
+const userSchema = mongoose.Schema(
+    {
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            minLength: 3
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        cart: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: '',
+            default: []
+        }],
+        orders: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: '',
+            default: []
+        }],
+        isadmin: Boolean,
+        contact: Number,
+        picture: String
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model('User',userSchema);
